1f27f852924d7a9c49ca3171d80f6334ec22ea0b
Uday Deshmukh <111646210+Udaypawar004@users.noreply.github.com>
2024-02-09 17:10:18 +0000

    made all essential react files and imported only important files in app js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { AppContext } from './context/contextApi.js'
 
+import header from "./components/header.jsx"
+import feed from "./components/feed.jsx"
+import searchResult from "./components/searchResult.jsx"
+import videoDetails from "./components/videoDetails.jsx"
+
 const App = () => {
   return (
     <AppContext>
