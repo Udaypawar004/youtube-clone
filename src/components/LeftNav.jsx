5762309a2490aa5aa2a3fcb41c566fc42624e23a
Uday Deshmukh <111646210+Udaypawar004@users.noreply.github.com>
2024-02-13 21:01:35 +0000

    Started building left nav

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { useNavigate } from 'react-router-dom'
+import leftNavMenuItem from "./LeftNavMenuItems"
+import { categories } from "../utils/constants"
+import { Context } from '../context/contextApi'
 
 const LeftNav = () => {
   return (
-    <div>LeftNav</div>
+    <div className="md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all">
+      <div className="flex px-5 flex-col">
+        {categories.map((item) => {
+          return (
+            <React.Fragment>
+              <leftNavMenuItem 
+                text={item.type === "home" ? "Home" : item.name}
+              />
+            </React.Fragment>
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
